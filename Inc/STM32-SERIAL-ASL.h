974e9dad17fade6a66375fb80c2dfee4978cc365
ansl <antonio.sanz.luengo@gmail.com>
2020-04-14 10:26:34 +0200

    restructure of the c++ lib

--- Inc/STM32-SERIAL-ASL.h
+++ Inc/STM32-SERIAL-ASL.h
@@ -1,37 +1,18 @@
-#ifndef STM32F1
-#define STM32F1
-#endif
-
-#ifndef STDARG_H
-#define STDARG_H
-#include <stdarg.h>
-#endif
-
-// #if defined(STM32F1)
-//  #include "stm32f1xx_hal.h"
-// 	#include <stm32_hal_legacy.h>
-// #elif defined(STM32F4)
-// 	#include "stm32f4xx_hal.h"
-// #else
-// 	#error "Error"
-// #endif
-
-#include "main.h"
-struct val_types{
-		int32_t a;
-		uint32_t b;
-		double c;
-	};
-enum format{_uint,_int,_float};
-enum disp_format{DEC,BIN,HEX};
-enum NL{x,NL};
-//  int Serial_print(UART_HandleTypeDef *huart,float num ,char* type[3]);
-int Serial_print(UART_HandleTypeDef *huart,...); //huart,format,value,disp_format
-//int Serial_print(UART_HandleTypeDef *huart,uint8_t num ,char* type[3]);
-//bool print(uint32_t num ,char* type[]=0);
-//bool println(char* string);
-//bool println(uint16_t num);	
 
 
+
+class Serial{
+	extern UART_HandleTypeDef Serial_huart;
+
+	public:
+	Serial();
+	bool begin(uint8_t port = 3, uint32_t baud = 115200);
+	bool print(char* string);
+	//bool print(uint8_t num ,char* type[]=0);
+	bool print(uint16_t num ,char* type[]=0);
+	//bool print(uint32_t num ,char* type[]=0);
+	//bool println(char* string);
+	//bool println(uint16_t num);	
+}
 
 
